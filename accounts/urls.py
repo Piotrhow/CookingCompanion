060890f9af4be92a93677b66afd5f8efcfe11453
Piotr <piotrhow@gmail.com>
2022-04-12 21:23:55 +0200

    Developed registration form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from accounts import views
+
 urlpatterns = [
-    #
-    #
-    #
+    path('', views.start_page, name='start_page'),
+    path('register/', views.register_page, name='register_page'),
+    path('register/thanks', views.thanks_page, name='thanks_page'),
+    path('login/', views.login_page, name='login_page'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+
+
+def start_page(request):
+    return render(request, 'accounts/start_page.html')
+
+
+def register_page(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password2')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return HttpResponseRedirect('thanks')
+        else:
+            print(form.errors)
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register_page.html', {'form': form})
+
+
+def thanks_page(request):
+    return render(request, 'accounts/thanks_page.html')
+
+
+def login_page(request):
+    return render(request, 'accounts/login_page.html')
