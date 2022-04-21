b6e6b28fa5f7961519f7db882fdc2096451bbeb0
kasia <mail@gmail.com>
2022-04-21 20:54:17 +0200

    Dodanie url dla aplikacji core

--- Config/urls.py
+++ Config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('', include('accounts.urls')),
     path('recipes/', include('recipes.urls')),
-    # path('core/', include('core.urls')),
+    path('core/', include('core.urls')),
     #
     #
     #
