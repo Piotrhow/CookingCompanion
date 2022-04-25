7df74fe7b14556e461cd81a4460f19491b5decc8
kasia <mail@gmail.com>
2022-04-25 19:11:29 +0200

    ZMIANY Z JERZYM.

--- core/views.py
+++ core/views.py
@@ -10,6 +10,7 @@ from recipes.models import Ingredient, IngredientCategory
 
 EMPTY_MSG = 'It looks like you don\'t have anything yet.'
 DUPLICATE_MSG = 'It looks like you already have that item. Try updating the existing amount instead.'
+WRONG_NAME_MSG = 'Something\'s wrong. Try again and make sure you don\'t make any typos.'
 
 
 # Create your views here.
@@ -38,45 +39,71 @@ def pantry_detail(request):
 
 # CREATE PANTRY INGREDIENT
 def pantryingredient_create(request):
-	# ingredient_category = (request.POST.get('ingredient_category')) #pobieranie od usera
-
-	ingredient_name = (request.POST.get('ingredient_name')) #pobieranie od usera
-	quantity = request.POST.get('quantity') #pobieranie od usera
 
 	id = request.user.id
 	p = get_object_or_404(Pantry, user_id=id)
 	pantry_id = p.id
-	pi_all = PantryIngredient.objects.filter(pantry_id=pantry_id)
-	flag = 0
+	pi_all = PantryIngredient.objects.filter(pantry_id=p.id)
 	ingredients = Ingredient.objects.all()
-
-	if ingredient_name and quantity:
-		ingredient = Ingredient.objects.filter(name=ingredient_name.lower()).first()
-		ingredient_id = ingredient.id
-		# category = IngredientCategory.objects.filter(name=ingredient_category.lower()).first()
-		# category_id = category.id
-		try:
-			PantryIngredient.objects.create(
-			pantry_id=pantry_id,
-			quantity=quantity,
-			ingredient_id=ingredient_id,
-			)
-			return redirect('core:pantry-detail')
-		except IntegrityError:
-			flag = 1
 	categories_all = IngredientCategory.objects.all()
-	return render(
-		request,
-		'core/pantryingredient_form.html',
-		context={
-			'pantry_ingredients': pi_all, #jeżeli nie będzie tego - to nie pokaże listy
-			'empty_msg': EMPTY_MSG,
-			'duplicate_msg': DUPLICATE_MSG,
-			'flag': flag,
-			'ingredients': ingredients,
-			'categories_all': categories_all,
-		}
-	)
+
+	# flag = 0
+
+	if request.method == "POST":
+
+		ingredient_name = (request.POST.get('ingredient_name')) #pobieranie od usera
+		quantity = request.POST.get('quantity') #pobieranie od usera
+
+		if ingredient_name and quantity:
+			ingredient = Ingredient.objects.filter(name=ingredient_name.lower()).first()
+			try:
+				ingredient_id = ingredient.id
+				PantryIngredient.objects.create(
+				pantry_id=pantry_id,
+				quantity=quantity,
+				ingredient_id=ingredient_id,
+				)
+				return redirect('core:pantry-detail')
+			except IntegrityError:
+				flag = 1
+			except AttributeError:
+				flag = 2
+			res = redirect('core:create')
+			res.set_cookie("flag", flag)
+			return res
+
+	else:
+
+		ingredient_category = (request.GET.get('ingredient_category')) #pobieranie od usera
+
+		flag = request.COOKIES.get("flag", 0)
+
+		if flag:
+			flag = int(flag)
+
+		if ingredient_category:
+			ingredients = Ingredient.objects.filter(category__name=ingredient_category)
+
+
+		res = render(
+			request,
+			'core/pantryingredient_form.html',
+			context={
+				'pantry_ingredients': pi_all, #jeżeli nie będzie tego - to nie pokaże listy
+				'categories_all': categories_all, #jeżeli nie będzie tego - to nie pokaże listy
+				'empty_msg': EMPTY_MSG,
+				'duplicate_msg': DUPLICATE_MSG,
+				'wrong_name_msg': WRONG_NAME_MSG,
+				'flag': flag,
+				'ingredients': ingredients,
+
+			}
+		)
+
+		res.delete_cookie("flag")
+
+		return res
+
 
 # DELETE AN INGREDIENT
 def pantryingredient_delete(request, pk):
