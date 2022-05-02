fd1a7e6fc7d4012f4269c0a99ae948e8fff1edf1
kasia <mail@gmail.com>
2022-05-02 23:49:44 +0200

    Poprawienie wyglądu html i niektórej funkcjonalności przed code review

--- recipes/models.py
+++ recipes/models.py
@@ -22,16 +22,16 @@ class Recipe(models.Model):
 	def __str__(self):
 		return f'{self.name}'
 
-	# resizing images
-	def save(self, *args, **kwargs):
-		super().save()
-
-		img = Image.open(self.picture.path)
-
-		if img.height > 250 or img.width > 250:
-			new_img = (250, 250)
-			img.thumbnail(new_img)
-			img.save(self.picture.path)
+	# # resizing images - WYŁĄCZONE
+	# def save(self, *args, **kwargs):
+	# 	super().save()
+	#
+	# 	img = Image.open(self.picture.path)
+	#
+	# 	if img.height > 250 or img.width > 250:
+	# 		new_img = (250, 250)
+	# 		img.thumbnail(new_img)
+	# 		img.save(self.picture.path)
 
 
 class Ingredient(models.Model):
